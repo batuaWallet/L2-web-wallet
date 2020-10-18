41d2136dcef169be5331d90dc10862548e4fd8cd
shivani <gupta.shivani254@gmail.com>
2020-10-18 22:13:21 +0530

    ✨ add address selection

--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -1,27 +1,80 @@
 import React, { useState, useContext } from "react";
-import { Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Tab,
+  Tabs,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { TabContext, TabPanel } from "@material-ui/lab";
+import {
+  CropFree as ScanIcon,
+  ImportContacts as ContactsIcon,
+} from "@material-ui/icons";
 import QrReader from "react-qr-scanner";
 
+const useStyles = makeStyles( theme => ({
+  appbar: {
+    flex: 1,
+  },
+  panel: {
+    marginTop: theme.spacing(8),
+  },
+  qrCode: {
+    marginRight: theme.spacing(65),
+  },
+  contacts: {
+    marginLeft: theme.spacing(65),
+  },
+}));
+
 export const Send = (props: any) => {
+  const classes = useStyles();
   const scanner = {
     height: 360,
     width: 522,
   };
-  const [scan, setScan] = useState("");
+  const [scan, setScan] = useState();
+  const [addressOpt, setAddressOpt] =useState("qrCode");
 
+  const updateSelection = (event: React.ChangeEvent<{}>, selectedTab: string) => {
+    setAddressOpt(selectedTab);
+  };
   const handleScan = (data: any) => {
     setScan(data);
   };
 
   return (
     <>
-      <Typography> Sending Payment </Typography>;
-      <QrReader
-        delay={100}
-        style={scanner}
-        onError={(err: any) => console.log(err)}
-        onScan={handleScan}
-      />
+      <TabContext value={addressOpt}>
+        <AppBar position="fixed" className={classes.appbar}>
+          <Tabs
+            value={"qrCode"}
+            onChange={updateSelection}
+            indicatorColor="secondary"
+            textColor="secondary"
+            variant="fullWidth"
+          >
+            <Tab value="qrCode" icon={<ScanIcon />} />
+            <Tab value="contacts" icon={<ContactsIcon />} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value="qrCode" className={classes.panel}>
+          { scan
+            ? <Typography> {scan} </Typography>
+            : <QrReader
+              delay={100}
+              style={scanner}
+              onError={(err: any) => console.log(err)}
+              onScan={handleScan}
+            />
+          }
+        </TabPanel>
+        <TabPanel value="contacts" className={classes.panel}>
+          Contacts
+        </TabPanel>
+        <Typography> Sending Payment </Typography>
+      </TabContext>
     </>
   )
 };
