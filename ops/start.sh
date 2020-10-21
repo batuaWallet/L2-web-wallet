0072b7c1df44c1b2f91d82bdb6b5e16b669cba85
bohendo <bohende@gmail.com>
2020-10-21 11:43:13 +0530

    🔧 run prod server in the background & fix final bugs

--- ops/start.sh
+++ ops/start.sh
@@ -3,9 +3,8 @@
 mkdir -p .letsencrypt
 
 docker run \
-  -it \
   --volume="$(pwd)/.certs:/etc/letsencrypt" \
   --env-file=.env \
   --publish="80:80" \
   --publish="443:443" \
-  batua_wallet_webserver:latest
+  batua_wallet_webserver:latest &
