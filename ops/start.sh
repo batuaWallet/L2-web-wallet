429a11eedb2a7796dca0910ccbfb88726f117fd3
bohendo <bohende@gmail.com>
2020-10-21 11:21:55 +0530

    🔧 expose 80 & 443 in prod mode

--- ops/start.sh
+++ ops/start.sh
@@ -2,4 +2,10 @@
 
 mkdir -p .letsencrypt
 
-docker run -it --volume="$(pwd)/.certs:/etc/letsencrypt" --env-file .env batua_wallet_webserver:latest
+docker run \
+  -it \
+  --volume="$(pwd)/.certs:/etc/letsencrypt" \
+  --env-file=.env \
+  --publish="80:80" \
+  --publish="443:443" \
+  batua_wallet_webserver:latest
